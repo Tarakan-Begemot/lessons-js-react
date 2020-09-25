c891d65491b58401c80478d47d34d8ef9ad2d063
laird <laird@mail.com>
2020-09-25 22:53:04 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// 'use strict';
+
+// console.log('bala-bala');
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+// console.log(arr[1]);
+
+
+// const answer = prompt('Вам есть 18?', '');
+// console.log(typeof (answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Какая у вас фамилия?', '');
+// answers[2] = prompt('Возраст?', '');
+
+// document.write(answers);
+// document.write(typeof(answers));
+
+// console.log(answers);
+
+const category = 'toys';
+
+console.log('https://someurl.com/' + category);
